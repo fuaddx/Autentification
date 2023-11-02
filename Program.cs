bb8a243b3dd568adfc2bfda7fbcbb43d47c9ce48
fuaddx <mi848xfcl@code.edu.az>
2023-11-02 13:25:42 +0400

    first commit

--- Program.cs
+++ Program.cs
@@ -1,106 +1,66 @@
-﻿using Core.Models;
-using System.Xml.Serialization;
+﻿using System.Text.RegularExpressions;
 
 namespace ConsoleApp2
 {
-    public class Program
+    internal class Program
     {
-
-
-        
         static void Main(string[] args)
         {
             User user = new User();
-
-
-            string name;
-            string surname;
-            string username;
-            string password;
-
             while (true)
             {
-                Console.WriteLine("1 -> Login\n2-> Register\n3->GetUsers\n4->Logout");
-                byte option = 0;
-                option = Convert.ToByte(Console.ReadLine());
-                switch (option)
+                Again:
+                try
                 {
-                    case 1:
-                        Console.WriteLine("Username:");
-                         username = Console.ReadLine();
-                        Console.WriteLine("Password:");
-                         password = Console.ReadLine();
-                        Authorization.Login(username, password);
-                        Console.WriteLine("Logged in");
-                        break;
-                    case 2:
-                        Console.WriteLine("Name:");
-                        name = Console.ReadLine();
-                        Console.WriteLine("Surname:");
-                         surname= Console.ReadLine();
-                        Console.WriteLine("Username:");
-                         username = Console.ReadLine();
-                        Console.WriteLine("Password:");
-                        password = Console.ReadLine();
-                        Console.WriteLine("Registered");
-                        Authorization.Register(name, surname, username, password);
-                     break;
-                    case 3:
-                        Authorization.GetUsers();
-                        break;
-                    case 4:
-                        Authorization.Logout();
-                    break;
+                    Console.WriteLine("Enter name:");
+                    string userinputname = Console.ReadLine();
+                    user.Name = userinputname;
                 }
-            }
-
-        }
-        public static List<User> users { get; set; } = new List<User>() ;
-        //public static User LogedIn { get; set; }
-
-    public static class Authorization
-    {
-        public static void Register(string name, string surname, string username, string password)
-        {
-            User user = new User()
-            {
-                Name = name,
-                Surname = surname,
-                UserName = username,
-                Password = password
-            };
-                 users.Add(user);
-            }
-
-
-        public static void Login(string username, string password)
-        {
-            foreach (var item in  users)
-            {
-                if (item.UserName == username && item.Password == password)
+                catch (InvalidNameException ex)
                 {
-                    Console.WriteLine("Logged in");
+                    Console.WriteLine(ex.Message);
+                    goto Again;
                 }
-            }
-        }
-
-
-            public static void GetUsers()
-            {
-                foreach (var item in  users)
+                Again2:
+                try
                 {
-                    Console.WriteLine(item.UserName);
+                    Console.WriteLine("Enter age:");
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    user.Age = age;
+                    
                 }
 
-            }
-        public static void Logout()
-        {   
+                catch (InvalidAgeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                 goto Again2;
+                }
+                Again3:
+                try
+                {
+                    Console.WriteLine("Enter phonenumber:");
+                    string phonenumber = Console.ReadLine();
+                    user.PhoneNumber = phonenumber;
+                }
+                catch (InvalidPhoneFormat ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    goto Again3;
+                }
+                Again4:
+                try
+                {
+                    Console.WriteLine("Enter password:");
+                    string password = Console.ReadLine();
+                    user.Password = password;
+                }
+                catch (PasswordException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    goto Again4;
+                }
+            } 
             
         }
-        
-
-
-
     }
 }
-    }
